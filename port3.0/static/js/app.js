fa2597a74e917b1d31935e404a38449d1e57ffc3
Gage Lieble <gagelieble@hotmail.com>
2023-10-20 23:04:02 -0700

    Testing new freelance port

--- port3.0/static/js/app.js
+++ port3.0/static/js/app.js
@@ -1,51 +1,43 @@
 // Scroll text
 
-let prevScroll = 0
+// let prevScroll = 0
 
-const isScrollingDown = () => {
-    let scrollDirection = null
-    let scrollPos = window.pageYOffset
+// const isScrollingDown = () => {
+//     let scrollDirection = null
+//     let scrollPos = window.pageYOffset
 
-    if(scrollPos > prevScroll){
-        scrollDirection = 'down'
-    } else if (scrollPos < prevScroll){
-        scrollDirection = 'up'
-    }
+//     if(scrollPos > prevScroll){
+//         scrollDirection = 'down'
+//     } else if (scrollPos < prevScroll){
+//         scrollDirection = 'up'
+//     }
 
-    prevScroll = scrollPos
+//     prevScroll = scrollPos
 
-    return scrollDirection
-}
+//     return scrollDirection
+// }
 
-const handleScroll = () => {
-    const textWrap = document.getElementById('scroll-text-wrap')
-    if(isScrollingDown() === 'down'){
-        // textWrap.style.animationDirection = 'reverse';
-        textWrap.classList.remove('RightToLeft')
-        textWrap.classList.add('LeftToRight')
-        console.log('scrolling down')
-    }else{
-        textWrap.classList.remove('LeftToRight')
-        textWrap.classList.add('RightToLeft')
-        console.log('scrolling up')
-    }
-}   
-window.addEventListener('scroll', handleScroll)
+// const handleScroll = () => {
+//     const textWrap = document.getElementById('scroll-text-wrap')
+//     if(isScrollingDown() === 'down'){
+//         // textWrap.style.animationDirection = 'reverse';
+//         textWrap.classList.remove('RightToLeft')
+//         textWrap.classList.add('LeftToRight')
+//         console.log('scrolling down')
+//     }else{
+//         textWrap.classList.remove('LeftToRight')
+//         textWrap.classList.add('RightToLeft')
+//         console.log('scrolling up')
+//     }
+// }   
 
 
 // Nav pill
 
 const pillMover = (location) => {
-    console.log(location)
-
     const pill = document.getElementById('nav-pill')
     const addClass = (loc) =>{ 
-        const classes = [' nav-item ', ' nav-pill ']
-        classes.forEach((cls) => {
-            
-            pill.classList += cls
-            
-        })
+        pill.classList.add('nav-pill')
         pill.classList.add(loc)
         return pill
     }
@@ -63,3 +55,54 @@ const pillMover = (location) => {
         addClass('nav-about')
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// window.addEventListener('scroll', () => {
+//     console.log('SCROLL')
+//     let count = 0
+//     if(count % 12 === 0){
+//         document.getElementById('text-roller').classList.add('speed-text')
+//     }
+
+//     setTimeout(() => {
+//         document.getElementById('text-roller').classList.remove('speed-text')
+//     },3000)
+//     count ++
+// })
+
+
+// var element = document.getElementById("text-roller");
+// var text_roller = new TextRoller(element);
+
+// text_roller.start();
+
+
+
+
+// window.TEGMUp = new TEGMarquee({
+//     marqueeSelector : '#example',
+//     direction : TEGMarquee.D_LEFT,
+//     mousePause : false,
+//   });
+
+//   TEGMUp.doScroll();
+
+
+const circle = new CircleType(document.getElementById('circle-text'));
+
+// circle.radius(1000).dir(-1);
+circle.radius(100).dir(-1)
+
+console.log(circle)
+// circle.container.classList.add('rotate')
