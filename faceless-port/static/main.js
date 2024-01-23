769b28d1e39cfab0964d84669dc67dd1db9fc3db
Gage Lieble <gagelieble@hotmail.com>
2024-01-23 02:19:14 -0800

    img

--- faceless-port/static/main.js
+++ faceless-port/static/main.js
@@ -1,12 +1,22 @@
+// CONTROLS
+let parallaxCursor = false
+let parallaxScroll = true
 
+// MOJI PARALLAX CURSOR
 
-// MOJI PARALLAX
+if (parallaxCursor){
 
-let parallax = false
-if (parallax){
+    let layers = document.querySelectorAll(".parallax")
+    document.addEventListener("mouseleave", () => {
+        console.log('nmindfg')
+        layers.forEach(layer => {
+            layer.style.transition  = `.1s ease`
+            
+            layer.style.transform  = ``
+        })
+    })
     
     document.addEventListener("mousemove", (e) => {
-        let layers = document.querySelectorAll(".parallax")
     
         layers.forEach(layer => {
            
@@ -23,43 +33,49 @@ if (parallax){
     
 }
 
+// MOJI PARRALAX SCROLL
 
-setTimeout(() => {
-    let prevPos = 0
-    
-    const isScrollingDown = () => {
-        let goingDown = false
-    
-        let scrollPos = window.scrollY
-    
-        if(scrollPos > prevPos){
-            goingDown = true
-        }
-    
-        prevPos = scrollPos
-    
-        return goingDown
-    
-    }
-    
-    document.addEventListener('scroll', () => {
-            let moji = document.getElementById("moji-char")
-            let mojiPos = Number(moji.getAttribute('data-pos'))
-            let vel = .5
-            
-            if (isScrollingDown()) {
-                mojiPos = mojiPos-vel
-                moji.style.transform = ` translateY(${mojiPos}px)`
-                moji.setAttribute('data-pos', mojiPos-vel)
-                
-            } else {
-            
-                mojiPos = vel * (mojiPos+vel)
-                moji.style.transform = ` translateY(${mojiPos}px)`
-                moji.setAttribute('data-pos', mojiPos+(vel+.5))
+if (parallaxScroll) {
+
+    setTimeout(() => {
+        let prevPos = 0
+        
+        const isScrollingDown = () => {
+            let goingDown = false
+        
+            let scrollPos = window.scrollY
+        
+            if(scrollPos > prevPos){
+                goingDown = true
             }
-            
-            
-    })
-
-}, 500)
+        
+            prevPos = scrollPos
+        
+            return goingDown
+        
+        }
+        
+        document.addEventListener('scroll', () => {
+                let moji = document.getElementById("moji-char")
+                let mojiPos = Number(moji.getAttribute('data-pos'))
+                let vel = 1
+                
+                if (isScrollingDown()) {
+                    mojiPos = mojiPos-vel
+                    moji.style.transform = ` translateY(${mojiPos}px)`
+                    moji.setAttribute('data-pos', mojiPos)
+                    
+                } else {
+                    if (mojiPos < 23) {
+                        
+                        mojiPos = mojiPos+vel
+                        moji.style.transform = ` translateY(${mojiPos}px)`
+                        moji.setAttribute('data-pos', mojiPos+vel)
+                    }
+                }
+                
+                
+        })
+    
+    }, 500)
+}
